fde1d58b70c598f2033f435e7d6c8ec5012eed5a
Jonghyun <nmj21c@gmail.com>
2020-01-09 17:44:08 +0900

    add option

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
 import * as path from "path";
 import { GraphQLServer } from "graphql-yoga";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+import session from "express-session";
+import ms from "ms";
 
 const allTypes = fileLoader(path.join(__dirname, "./graphql/**/schema*.js"));
 const allResolvers = fileLoader(path.join(__dirname, "./graphql/**/resolvers*.js"));
 
-const server: GraphQLServer = new GraphQLServer({
-    typeDefs: mergeTypes(allTypes),
-    resolvers: mergeResolvers(allResolvers)
+const context = req => ({
+    req: req.request
 });
 
-server.start(() => console.log("Graphql server Running at localhost:4000"));
+const opts = {
+    port: 4000,
+    cors: {
+        credentials: true,
+        origin: ['http://localhost:8080']
+    }
+}
+
+const server: GraphQLServer = new GraphQLServer({
+    typeDefs: mergeTypes(allTypes),
+    resolvers: mergeResolvers(allResolvers),
+    context: context
+});
+
+// console.log(`node_env \ ${process.env.NODE_ENV}`);
+// server.express.use(session({
+//     name: 'qid',
+//     secret: '',
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: {
+//         secure: false,
+//         maxAge: ms('1d')
+//     }
+// }));
+
+server.start(opts, () => console.log("Graphql server Running at localhost:4000"));
